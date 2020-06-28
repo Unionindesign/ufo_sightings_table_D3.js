e619d50b71331d7ea581602c5e3382de5657d4c4
stephen_marshall <stephen.marshall@fareharbor.com>
2020-06-28 16:45:46 -0600

    have table working with rows appended and some styles. Starting on bonus table filters.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Data: ", data)
+var tbody = d3.select("tbody");
+
+data.forEach((ufo) => {
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
